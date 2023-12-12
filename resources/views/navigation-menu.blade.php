e7b7b9780de25940b1d2218a6afac1e9ebc6b474
Ennonn <aljonfernando09@gmail.com>
2023-12-13 02:17:58 +0800

    ToDoList

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,8 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-nav-link href="{{ route('dashboard') }}" wire:navigate :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('task.index') }}" wire:navigate :active="request()->routeIs('task.index')">
+                        {{ __('To Do List') }}
                     </x-nav-link>
                 </div>
             </div>
